0ecb74587f5ed9b89d494a7095c218c83e5342ea
Keith Holler <robert.keith.holler@gmail.com>
2020-09-18 08:39:33 -0400

    React Router Parameters

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,39 +1,34 @@
 import React from "react";
-import {
-  Card,
-  CardImg,
-  CardImgOverlay,
-  CardTitle,
-} from "reactstrap";
+import { Link } from "react-router-dom";
+import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
 // import CampsiteInfo from "./CampsiteInfoComponent";
 
-function RenderDirectoryItem({campsite,onClick}){
-  return(
+function RenderDirectoryItem({campsite}) {
+  return (
       <Card>
+          <Link to={`/directory/${campsite.id}`}>
               <CardImg width="100%" src={campsite.image} alt={campsite.name} />
               <CardImgOverlay>
-                <CardTitle>{campsite.name}</CardTitle>
+                  <CardTitle>{campsite.name}</CardTitle>
               </CardImgOverlay>
-            </Card>
+          </Link>
+      </Card>
   );
 }
 function Directory(props) {
-
-    const directory = props.campsites.map((campsite) => {
-      return (
-        <div key={campsite.id} className="col-md-5 m-1">
-          <RenderDirectoryItem campsite={campsite}/>
-        </div>
-      );
-    });
-
+  const directory = props.campsites.map((campsite) => {
     return (
-      <div className="container">
-        <div className="row ">{directory}</div>
-        
+      <div key={campsite.id} className="col-md-5 m-1">
+        <RenderDirectoryItem campsite={campsite} />
       </div>
     );
-  }
+  });
 
+  return (
+    <div className="container">
+      <div className="row ">{directory}</div>
+    </div>
+  );
+}
 
 export default Directory;
